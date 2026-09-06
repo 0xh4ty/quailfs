5b1d4906e71de258b16b38d807494be1e9318237
0xh4ty <0xh4ty@proton.me>
2026-09-06 11:53:37 +0530

    Add client and node mode entry point

--- cmd/quailfs/main.go
+++ cmd/quailfs/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	fmt.Println("QuailFS")
+
+	switch {
+	case len(os.Args) == 1:
+		fmt.Println("Starting Client...")
+
+	case os.Args[1] == "--node":
+		fmt.Println("Starting Node...")
+
+	default:
+		fmt.Println("Unknown argument:", os.Args[1])
+		fmt.Println("Usage: quailfs [--node]")
+		os.Exit(1)
+	}
 }
