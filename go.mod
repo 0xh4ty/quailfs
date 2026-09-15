c9d8d4bcb4867c8383873a5aa389215ac931db49
0xh4ty <0xh4ty@proton.me>
2026-09-15 19:50:03 +0530

    Add bbolt storage layer

--- go.mod
+++ go.mod
@@ -7,10 +7,11 @@ require (
 	github.com/klauspost/compress v1.20.0
 	github.com/klauspost/reedsolomon v1.14.2
 	github.com/tyler-smith/go-bip39 v1.1.0
+	go.etcd.io/bbolt v1.5.0
 	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
 )
 
 require (
 	github.com/klauspost/cpuid/v2 v2.3.0 // indirect
-	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/sys v0.45.0 // indirect
 )
---
+++ internal/storage/bbolt.go
@@ -0,0 +1,84 @@
+package storage
+
+import (
+	"errors"
+
+	"go.etcd.io/bbolt"
+)
+
+func InitializeDatabase() (*bbolt.DB, error) {
+	db, err := bbolt.Open("node-data/quailfs.db", 0600, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Update(func(tx *bbolt.Tx) error {
+		_, err := tx.CreateBucketIfNotExists([]byte("inventory"))
+		if err != nil {
+			return err
+		}
+
+		_, err = tx.CreateBucketIfNotExists([]byte("catalogs"))
+		if err != nil {
+			return err
+		}
+
+		_, err = tx.CreateBucketIfNotExists([]byte("ads"))
+		if err != nil {
+			return err
+		}
+
+		_, err = tx.CreateBucketIfNotExists([]byte("peers"))
+		if err != nil {
+			return err
+		}
+
+		_, err = tx.CreateBucketIfNotExists([]byte("claims"))
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func Put(db *bbolt.DB, bucketName string, key []byte, value []byte) error {
+	err := db.Update(func(tx *bbolt.Tx) error {
+		bucket := tx.Bucket([]byte(bucketName))
+		if bucket == nil {
+			return errors.New("bucket not found")
+		}
+		return bucket.Put(key, value)
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func Get(db *bbolt.DB, bucketName string, key []byte) ([]byte, error) {
+	var value []byte
+	err := db.View(func(tx *bbolt.Tx) error {
+		bucket := tx.Bucket([]byte(bucketName))
+		if bucket == nil {
+			return errors.New("bucket not found")
+		}
+
+		value = append([]byte(nil), bucket.Get(key)...)
+
+		if value == nil {
+			return nil
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return value, nil
+}
